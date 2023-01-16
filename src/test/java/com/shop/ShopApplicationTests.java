be0f7d3eb95312a4868f7777986e7927827ae56a
KimGeonWoo <ikkw2040@naver.com>
2023-01-17 00:58:32 +0900

    테스트 환경 설정

--- src/test/java/com/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/ShopApplicationTests.java
@@ -1,13 +1,40 @@
 package com.shop;
 
+import com.shop.constant.ItemSellStatus;
+import com.shop.entity.Item;
+import com.shop.repository.ItemRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class ShopApplicationTests {
 
+	@Autowired
+	ItemRepository itemRepository;
+
 	@Test
-	void contextLoads() {
+	@DisplayName("상품저장테스트")
+	public void createItemTest(){
+
+		Item item = new Item();
+
+		item.setItemName("테스트 상품");
+		item.setPrice(10000);
+		item.setItemDetail("테스트상품상세설명");
+		item.setItemSellStatus(ItemSellStatus.SELL);
+		item.setStockNumber(100);
+		item.setRegTime(LocalDateTime.now());
+		item.setUpdateTime(LocalDateTime.now());
+
+		Item savedItem = itemRepository.save(item);
+
+		System.out.println(savedItem.toString());
 	}
 
 }
---
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.shop.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+
+}
