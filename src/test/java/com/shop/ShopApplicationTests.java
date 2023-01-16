2b2bee587560cbb28a599e0ba64f370df95aabe9
KimGeonWoo <ikkw2040@naver.com>
2023-01-17 01:30:03 +0900

    조회 쿼리

--- src/test/java/com/shop/ShopApplicationTests.java
+++ src/test/java/com/shop/ShopApplicationTests.java
@@ -3,6 +3,7 @@ package com.shop;
 import com.shop.constant.ItemSellStatus;
 import com.shop.entity.Item;
 import com.shop.repository.ItemRepository;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations="classpath:application-test.properties")
@@ -37,4 +39,28 @@ class ShopApplicationTests {
 		System.out.println(savedItem.toString());
 	}
 
+
+	public void createItemList(){
+		for(int i=0; i<=10; i++){
+			Item item = new Item();
+			item.setItemName("테스트 상품" + i);
+			item.setPrice(10000 + i);
+			item.setItemDetail("테스트 상품 상세 설명" + i);
+			item.setItemSellStatus(ItemSellStatus.SELL);
+			item.setStockNumber(100);
+			item.setRegTime(LocalDateTime.now());
+			item.setUpdateTime(LocalDateTime.now());
+			Item savedItem = itemRepository.save(item);
+		}
+	}
+
+	@Test
+	@DisplayName("상품명 조회 테스트")
+	public void findByItemNameTest(){
+		this.createItemList();;
+		List<Item> itemList =itemRepository.findByItemName("테스트 상품1");
+		for(Item item : itemList){
+			System.out.println(item.toString());
+		}
+	}
 }
