d3203c90d4882cb39ea42ac000d42ebbec05b0cf
KimGeonWoo <ikkw2040@naver.com>
2023-01-28 20:41:35 +0900

    th:each

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -28,5 +30,24 @@ public class ThymeleafExController {
 
         model.addAttribute("itemDto", itemDto);
         return "thymeleafEX/thymeleafEx02";
+    }
+
+    @GetMapping("/ex03")
+    public String thymeleafExample03(Model model){
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for(int i=1; i<=10; i++){
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품상세설명" + i);
+            itemDto.setItemName("테스트 상품" + i);
+            itemDto.setPrice(1000 * i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEX/thymeleafEx03";
     }
 }
