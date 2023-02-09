aeacc604091185d555d8d1188cdb7a1be31cb8e1
KimGeonWoo <ikkw2040@naver.com>
2023-02-09 23:31:47 +0900

    스프링 시큐리티 설정

---
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.shop.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.wes
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")
+                .permitAll()
+                .defaultSuccessUrl("/")
+                .usernameParameter("memberid")
+                .failureUrl("/members/login/error")
+                .and()
+                .logout()
+                .logoutUrl("/members/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+
+        ;
+
+        http.authorizeHttpRequests((requests) ->requests
+                .requestMatchers("/css/**", "/js/**", "/img/**", "/fragments/**", "/layouts/**", "/favicon.ico", "/resources/**", "/error").permitAll()
+                .requestMatchers("/", "/members/**", "/item/**", "/images/**", "/board/**", "/category/**").permitAll()
+                .requestMatchers("/order/**").hasRole("USER")
+                .requestMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().authenticated())
+        ;
+
+//        http.exceptionHandling()
+//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+//        ;
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
