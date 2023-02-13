50367898632994d79dca37a399daa52f56c6b77e
KimGeonWoo <ikkw2040@naver.com>
2023-02-14 01:30:49 +0900

    중복가입테스트

--- src/test/java/com/shop/service/MemberServiceTest.java
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.TestPropertySource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
@@ -22,7 +24,7 @@ public class MemberServiceTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public Member createMember(){
+    public Member createMember() {
         MemberFormDto memberFormDto = new MemberFormDto();
         memberFormDto.setEmail("test@email.com");
         memberFormDto.setName("홍길동");
@@ -33,8 +35,8 @@ public class MemberServiceTest {
 
     @Test
     @DisplayName("회원가입 테스트")
-    public void saveMemberTest(){
-        Member member   = createMember();
+    public void saveMemberTest() {
+        Member member = createMember();
         Member savedMember = memberService.saveMember(member);
 
         assertEquals(member.getEmail(), savedMember.getEmail());
@@ -43,4 +45,19 @@ public class MemberServiceTest {
         assertEquals(member.getPassword(), savedMember.getPassword());
         assertEquals(member.getRole(), savedMember.getRole());
     }
+
+    @Test
+    @DisplayName("중복가입 테스트")
+    public void saveDuplicateMemberTest() {
+        Member member1 = createMember();
+        Member member2 = createMember();
+        memberService.saveMember(member1);
+
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            memberService.saveMember(member2);
+        });
+        assertEquals("이미 가입된 회원입니다.", e.getMessage());
+    }
+
+
 }
