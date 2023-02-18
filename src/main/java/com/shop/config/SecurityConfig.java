66599656fa0341fd35862cce4663e275d7fd2f1f
KimGeonWoo <ikkw2040@naver.com>
2023-02-18 22:23:35 +0900

    코드 수정

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.shop.config;
 
-
 import com.shop.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -11,27 +10,24 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     @Autowired
     MemberService memberService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
         http.formLogin()
                 .loginPage("/members/login")
-                .permitAll()
                 .defaultSuccessUrl("/")
-                .usernameParameter("memberid")
+                .usernameParameter("email")
                 .failureUrl("/members/login/error")
                 .and()
                 .logout()
-                .logoutUrl("/members/logout")
                 .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
-
+                .logoutSuccessUrl("/")
         ;
 
         http.authorizeRequests()
@@ -39,12 +35,11 @@ public class SecurityConfig {
                 .mvcMatchers("/", "/members/**", "/item/**", "/images/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
-                ;
+        ;
 
 //        http.exceptionHandling()
 //                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
 //        ;
-
         return http.build();
     }
 
