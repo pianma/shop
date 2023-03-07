41c5807ca43bb7d6b00ab6e3f7545e7632222e07
KimGeonWoo <ikkw2040@naver.com>
2023-03-07 23:46:26 +0900

    페이지 권한 설정 진행중

---
+++ src/main/java/com/shop/config/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,13 @@
+package com.shop.config;
+
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, a)
+}
