a6e42684642d225ab249753e18b43ffd39253030
KimGeonWoo <ikkw2040@naver.com>
2023-03-23 23:39:30 +0900

    상품 등록

--- src/main/java/com/shop/dto/ItemFormDto.java
+++ src/main/java/com/shop/dto/ItemFormDto.java
@@ -18,7 +18,7 @@ public class ItemFormDto {
     private Long id;
 
     @NotBlank(message = "상품명은 필수 입력 값입니다.")
-    private String itemNm;
+    private String itemName;
 
     @NotNull(message = "가격은 필수 입력 값입니다.")
     private Integer price;
