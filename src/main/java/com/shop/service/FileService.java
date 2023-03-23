6191b5f1c7105d4487d546c07661c5cbadb4f31a
KimGeonWoo <ikkw2040@naver.com>
2023-03-23 21:42:36 +0900

    fileService

---
+++ src/main/java/com/shop/service/FileService.java
@@ -0,0 +1,35 @@
+package com.shop.service;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
+
+@Service
+@Log
+public class FileService {
+
+    public String uploadFile(String uploadPath, String originalFileName, byte[] fileData) throws Exception{
+        UUID uuid = UUID.randomUUID();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String savedFileName = uuid.toString() + extension;
+        String filUploadFullUrl = uploadPath + "/" + savedFileName;
+        FileOutputStream fos = new FileOutputStream(filUploadFullUrl);
+        fos.write(fileData);
+        fos.close();
+        return savedFileName;
+    }
+
+    public void deleteFile(String filePath) throws Exception{
+        File deleteFile = new File(filePath);
+
+        if(deleteFile.exists()){
+            deleteFile.delete();
+            log.info("파일 삭제 완료");
+        }else{
+            log.info("파일 존재 하지 않음");
+        }
+    }
+}
